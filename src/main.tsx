4c400104fca85b1ae81407a9e9d7db6ca74fa5e4
ZERO-JIFAR <juanignacioarnold@gmail.com>
2024-10-12 12:44:52 -0300

    prueba de rama

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// prueba de rama
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
