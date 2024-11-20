4895af0ec977e77d0ac4ecc3bf8e85168dfa4d97
FreshEffect <gossuinulises@gmail.com>
2024-11-20 20:13:16 -0300

    Mejora Articulos

--- src/components/modals/CrearSucursal/CrearSucursal.tsx
+++ src/components/modals/CrearSucursal/CrearSucursal.tsx
@@ -22,7 +22,7 @@ interface ModalSucursalProps {
 
 export const CrearSucursal: React.FC<ModalSucursalProps> = ({ onClose, onAddSucursal }) => {
     const [sucursalData, setSucursalData] = useState<ISucursal>({
-        id: Date.now().toString(), // ID único basado en el timestamp
+        id: Date.now().toString(),
         nombre: "",
         apertura: "",
         cierre: "",
@@ -34,29 +34,25 @@ export const CrearSucursal: React.FC<ModalSucursalProps> = ({ onClose, onAddSucu
         codigoPostal: "",
     });
 
-    const [warningMessage, setWarningMessage] = useState<string>(""); // Estado para el mensaje de advertencia
-    const [imageSelected, setImageSelected] = useState<boolean>(false); // Estado para saber si se ha seleccionado una imagen
+    const [warningMessage, setWarningMessage] = useState<string>("");
+    const [imageSelected, setImageSelected] = useState<boolean>(false);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
-        setSucursalData({ ...sucursalData, [name]: value }); // Actualiza el estado con el valor del campo
+        setSucursalData({ ...sucursalData, [name]: value });
     };
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files.length > 0) {
-            setImageSelected(true); // Indica que se ha seleccionado una imagen
-        } else {
-            setImageSelected(false); // No se ha seleccionado imagen
-        }
+        setImageSelected(!!event.target.files?.length);
     };
 
     const handleConfirm = () => {
-        const allFieldsFilled = Object.values(sucursalData).every(field => field !== "") && imageSelected;
-    
+        const allFieldsFilled = Object.values(sucursalData).every(field => field.trim() !== "") && imageSelected;
+
         if (!allFieldsFilled) {
             setWarningMessage("Por favor, asegúrate de completar todos los campos.");
         } else {
-            setWarningMessage(""); // Limpiar el mensaje de advertencia
+            setWarningMessage("");
             onAddSucursal(sucursalData);
             onClose();
         }
@@ -75,29 +71,29 @@ export const CrearSucursal: React.FC<ModalSucursalProps> = ({ onClose, onAddSucu
     ];
 
     return (
-        <div className="modals">
-            <div className="card-sucursal">
-                <div className="card-body">
-                    <h5 className="card-title">Crear una Sucursal</h5>
-                    {warningMessage && <div className="alert alert-warning">{warningMessage}</div>} {/* Mensaje de advertencia */}
-                    <div className="container-sucursal">
-                        <div className="grid-container">
+        <div className="modal-sucursal">
+            <div className="card-sucursal-modal">
+                <div className="card-sucursal-body">
+                    <h5 className="card-sucursal-title">Crear una Sucursal</h5>
+                    {warningMessage && <div className="alert alert-warning">{warningMessage}</div>}
+                    <div className="container-sucursal-main">
+                        <div className="grid-container-sucursal">
                             {placeholders.map((item, index) => (
                                 <input
                                     key={index}
                                     name={item.name}
-                                    placeholder={item.placeholder} 
-                                    className="form-control"
-                                    style={{ marginBottom: "10px", textAlign:"center" }}
+                                    placeholder={item.placeholder}
+                                    className="form-control-sucursal"
+                                    style={{ marginBottom: "10px", textAlign: "center" }}
                                     onChange={handleChange}
                                 />
                             ))}
                         </div>
-                        <div className="img-buttons">
-                            <div className="input-group">
+                        <div className="img-buttons-sucursal">
+                            <div className="input-group-sucursal">
                                 <input
                                     type="file"
-                                    className="form-control"
+                                    className="form-control-sucursal"
                                     id="inputGroupFile"
                                     aria-label="Upload"
                                     onChange={handleFileChange}
@@ -107,9 +103,13 @@ export const CrearSucursal: React.FC<ModalSucursalProps> = ({ onClose, onAddSucu
                                     <FaImage />
                                 </span>
                             </div>
-                            <div className="buttons">
-                                <button type="button" className="btn btn-dark" onClick={handleConfirm}>Confirmar</button>
-                                <button type="button" className="btn btn-danger" onClick={onClose}>Cancelar</button>
+                            <div className="buttons-sucursal">
+                                <button type="button" className="btn btn-dark" onClick={handleConfirm}>
+                                    Confirmar
+                                </button>
+                                <button type="button" className="btn btn-danger" onClick={onClose}>
+                                    Cancelar
+                                </button>
                             </div>
                         </div>
                     </div>
